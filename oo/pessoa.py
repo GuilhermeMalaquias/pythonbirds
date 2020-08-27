d4c4005d8a4184575ab4c62ad0937357c7b1dd21
Guilherme Malaquias <silvaam.guilherme@gmail.com>
2020-08-27 17:42:46 -0300

    Atributo de instancias, nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,14 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
         return 'Teste'
 
 if __name__ == "__main__":
     pessoa = Pessoa()
     print(pessoa.cumprimentar())
+    print(pessoa.nome)
+    pessoa.nome = 'Guilherme'
+    print(f'Nome:{pessoa.nome} Idade: {pessoa.idade}')
