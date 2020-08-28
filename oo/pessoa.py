0fc1f8d21769d4635454778da79d2d1cbe5b45b0
Guilherme Malaquias <silvaam.guilherme@gmail.com>
2020-08-28 17:58:53 -0300

    Atributo Dinamico

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -10,7 +10,10 @@ class Pessoa:
 if __name__ == "__main__":
     guilherme = Pessoa(nome='Guilherme', idade=20)
     Edson = Pessoa(guilherme, nome='Edson', idade=51)
-    print(Pessoa.cumprimentar(Edson))
     print(Edson.cumprimentar())
     for filho in Edson.filhos:
         print(f'Nome: {Edson.nome}, Filho: {filho.nome}')
+    Edson.sobrenome = 'Silva'
+    print(Edson.sobrenome)
+    del Edson.sobrenome
+    print(Edson.__dict__)
