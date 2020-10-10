6930e12452b5209c795ac9a1cfedcb5cada7a303
Guilherme Malaquias <silvaam.guilherme@gmail.com>
2020-10-10 14:55:54 -0300

    Atributos de classes

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
     def __init__(self, *filhos, nome=None, idade=35):
         self.filhos = list(filhos)
         self.nome = nome
@@ -7,13 +8,20 @@ class Pessoa:
     def cumprimentar(self):
         return 'Ola'
 
+
 if __name__ == "__main__":
     guilherme = Pessoa(nome='Guilherme', idade=20)
+    Teste = Pessoa(nome='Teste', idade=20)
     Edson = Pessoa(guilherme, nome='Edson', idade=51)
-    print(Edson.cumprimentar())
-    for filho in Edson.filhos:
-        print(f'Nome: {Edson.nome}, Filho: {filho.nome}')
-    Edson.sobrenome = 'Silva'
-    print(Edson.sobrenome)
-    del Edson.sobrenome
-    print(Edson.__dict__)
+    for filhos in Edson.filhos:
+        print(filhos.nome)
+    guilherme.sobrenome = "Malaquias"
+    print(guilherme.sobrenome)
+    guilherme.olhos = 1
+    print(guilherme.olhos)
+    print(Edson.olhos)
+    Pessoa.olhos = 3
+    del guilherme.olhos
+    print(Pessoa.olhos, guilherme.olhos, Edson.olhos)
+
+
